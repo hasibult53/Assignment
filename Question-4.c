08c1279b86cd2950ffcbd4bc579f617afa245793
Hasibul Hassan <115937739+hasibult53@users.noreply.github.com>
2024-05-24 21:56:56 +0600

    Create Question-4.c

---
+++ Question-4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  int a;
+
+  printf("Enter an integer: ");
+  scanf("%d", &a);
+
+  if (a > 0) {
+    printf("%d is positive.\n", a);
+  } else if (a < 0) {
+    printf("%d is negative.\n", a);
+  } else {
+    printf("%d is zero.\n", a);
+  }
+
+  return 0;
+}
